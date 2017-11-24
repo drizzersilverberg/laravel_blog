b483db4ead1db8ec24fbb92ddc79864a6ff94871
Dendi Handian <dendihandian@gmail.com>
2017-11-24 20:19:45 +0000

    modify tasks routes into controller-mapping routes

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,7 @@
 <?php
 
 /* url: localhost:8000 */
-Route::get('tasks', function () {
-    # -- get all row from tasks table
-    $tasks = App\Task::all();
-    # -- return view including tasks
-    return view('tasks.index', compact('tasks'));
-});
-
+Route::get('tasks', 'TasksController@index');
 /* url: localhost:8000/tasks/{taskId} */
-Route::get('tasks/{task}', function ($id) {
-    # -- find a task by its id
-    $task = App\Task::find($id);
-    # -- return view including task
-    return view('tasks.show', compact('task'));
-});
+Route::get('tasks/{task}', 'TasksController@show');
+
