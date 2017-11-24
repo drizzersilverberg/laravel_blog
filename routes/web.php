dfb905a189fd600808531fa2e980e89228ccc206
Dendi Handian <dendihandian@gmail.com>
2017-11-24 20:02:37 +0000

    use eloquent to get tasks

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,9 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 /* url: localhost:8000 */
 Route::get('tasks', function () {
     # -- get all row from tasks table
-    $tasks = DB::table('tasks')->get();
+    $tasks = App\Task::all();
     # -- return view including tasks
     return view('tasks.index', compact('tasks'));
 });
@@ -22,7 +11,7 @@ Route::get('tasks', function () {
 /* url: localhost:8000/tasks/{taskId} */
 Route::get('tasks/{task}', function ($id) {
     # -- find a task by its id
-    $task = DB::table('tasks')->find($id);
+    $task = App\Task::find($id);
     # -- return view including task
     return view('tasks.show', compact('task'));
 });
