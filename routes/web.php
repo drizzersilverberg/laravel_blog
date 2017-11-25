6b8d22e3d580a39b715830dfd447820a369ee19e
Dendi Handian <dendihandian@gmail.com>
2017-11-25 12:54:00 +0000

    remove tasks routes

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,6 @@ Route::get('/', function(){
     return view('welcome');
 });
 
-/* url: localhost:8000/tasks */
-Route::get('tasks', 'TasksController@index');
-/* url: localhost:8000/tasks/{taskId} */
-Route::get('tasks/{task}', 'TasksController@show');
-
 /* url: localhost:8000/posts */
 Route::get('posts', 'PostsController@index');
 
