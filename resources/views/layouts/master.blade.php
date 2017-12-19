3aebfeabe45893a6d4dcb4056a4ba310b48335a0
Dendi Handian <dendihandian@gmail.com>
2017-12-19 08:06:17 +0000

    modify views to add auth check

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -19,7 +19,18 @@
 
   <body>
 
-    @include('layouts.nav')
+    <header>
+
+      @include('layouts.nav')
+
+      <div class="blog-header">
+        <div class="container">
+          <h1 class="blog-title">The Bootstrap Blog</h1>
+          <p class="lead blog-description">An example blog template built with Bootstrap.</p>
+        </div>
+      </div>
+
+    </header>
 
     <main class="container" role="main">
         <div class="row">
