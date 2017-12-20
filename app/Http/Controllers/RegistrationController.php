25b8e46f7478a07d0ec615da762786b62a62bc74
Dendi Handian <dendihandian@gmail.com>
2017-12-20 09:56:39 +0000

    add flash session message when a user is registered/created

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -15,6 +15,8 @@ class RegistrationController extends Controller
     {
         $request->persist();
 
+        session()->flash('message','Thanks so much for signing up!');
+
         return redirect()->home();
     }
 }
