392e93d09ba09a182e5c39c9be4b5a7494a3edb3
Dendi Handian <dendihandian@gmail.com>
2017-12-21 04:22:17 +0000

    add posts/tags/{tags} route

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,13 @@
 <?php
 
-/* url: localhost:8000 */
 Route::get('/', 'PostsController@index')->name('home');
 
-/* url: localhost:8000/posts */
 Route::get('posts', 'PostsController@index');
-
-Route::get('posts/create', 'PostsController@create');
-
 Route::post('posts', 'PostsController@store');
-
 Route::get('posts/{post}', 'PostsController@show');
-
 Route::post('posts/{post}/comments', 'CommentsController@store');
+Route::get('posts/create', 'PostsController@create');
+Route::get('posts/tags/{tag}', 'TagsController@index');
 
 Route::get('register', 'RegistrationController@create');
 Route::post('register', 'RegistrationController@store');
