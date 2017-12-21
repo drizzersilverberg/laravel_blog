292604a6fe12c4547a7112d8fd974c8c94dff3f4
Dendi Handian <dendihandian@gmail.com>
2017-12-21 04:13:52 +0000

    add Tags list

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -6,9 +6,17 @@
   <div class="sidebar-module">
     <h4>Archives</h4>
     <ol class="list-unstyled">
-
       @foreach ($archives as $item)
         <li><a href="/?month={{ $item['month']}}&year={{ $item['year'] }}">{{ $item['month'] . ' ' . $item['year'] }}</a></li>
+      @endforeach
+    </ol>
+  </div>
+  <div class="sidebar-module">
+    <h4>Tags</h4>
+    <ol class="list-unstyled">
+
+      @foreach ($tags as $tag)
+        <li><a href="/posts/tags/{{ $tag }}">{{ $tag }}</a></li>
       @endforeach
     </ol>
   </div>
